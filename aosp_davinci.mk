da3741c4e585dc6faa7e7d1706622ba5d6479e4a
Rozetka.img <klepikov383@gmail.com>
2020-02-18 18:13:25 +0300

    Rename Pixel Experience to Evolution X

--- aosp_davinci.mk
+++ aosp_davinci.mk
@@ -15,7 +15,7 @@
 
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common EvolutuonX stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
